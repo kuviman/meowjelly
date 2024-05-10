a95ee85c6eb4c673dbf52e03b0c001c4efbdbc9e
kuviman <kuviman@gmail.com>
2024-05-10 22:41:08 +0400

    digits in assets

--- src/assets.rs
+++ src/assets.rs
@@ -26,4 +26,6 @@ pub struct Assets {
     pub obstacles: Vec<Rc<ugli::Texture>>,
     pub player: Player,
     pub tutorial: Tutorial,
+    #[load(list = "0..=9", path = "digits/*.png")]
+    pub digits: Vec<ugli::Texture>,
 }
